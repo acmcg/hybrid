998fdf3a82564ab4b1d4460380a307e696ac4b26
McGregor <andrew.mcgregor@hpe.com>
2018-11-16 17:43:38 +1100

    v1

---
+++ Networking.tf
@@ -0,0 +1,25 @@
+resource "azurerm_resource_group" "test" {
+  name     = "acceptanceTestResourceGroup1"
+  location = "West US"
+}
+
+resource "azurerm_network_security_group" "test" {
+  name                = "acceptanceTestSecurityGroup1"
+  location            = "${azurerm_resource_group.test.location}"
+  resource_group_name = "${azurerm_resource_group.test.name}"
+}
+
+resource "azurerm_virtual_network" "Azure vNet" {
+  name                = "ApplicationvNet"
+  location            = "${azurerm_resource_group.test.location}"
+  resource_group_name = "${azurerm_resource_group.test.name}"
+  address_space       = ["10.100.102.9/23"]
+
+  subnet {
+    name           = "ApplicationSubnet"
+    address_prefix = "10.100.102.0/24"
+  }
+  tags {
+    environment = "Production"
+  }
+}
