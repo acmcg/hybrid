32126f0a7c1531c79771fee9287ae69b271c734a
McGregor <andrew.mcgregor@hpe.com>
2018-11-20 13:48:47 +1100

    Fixed terraform initialization

--- Networking.tf
+++ Networking.tf
@@ -13,7 +13,7 @@ resource "azurerm_network_security_group" "test" {
   resource_group_name = "${azurerm_resource_group.test.name}"
 }
 
-resource "azurerm_virtual_network" "Azure vNet" {
+resource "azurerm_virtual_network" "Azure_vNet" {
   name                = "ApplicationvNet"
   location            = "${azurerm_resource_group.test.location}"
   resource_group_name = "${azurerm_resource_group.test.name}"
